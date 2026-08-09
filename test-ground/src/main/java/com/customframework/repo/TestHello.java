b5fdaf8d6fa86bb31042f917ecf434fa7e2d4fec
Mambetkulov <muganm320@gmail.com>
2026-08-09 22:49:23 +0600

    anotation repo

--- test-ground/src/main/java/com/customframework/repo/TestHello.java
+++ test-ground/src/main/java/com/customframework/repo/TestHello.java
@@ -2,13 +2,15 @@ package com.customframework.repo;
 
 import com.customframework.Scope;
 import com.customframework.annotation.Component;
+import com.customframework.annotation.Inject;
 import com.customframework.annotation.ScoopScope;
 
 @ScoopScope(Scope.PROTOTYPE)
 @Component
 public class TestHello {
 
-    public TestHello() {
-        System.out.println("TestHello");
+    @Inject
+    public TestHello(TestRepository testRepository) {
+        System.out.println("😼TestHello : this is testRepository : " + testRepository);
     }
 }
